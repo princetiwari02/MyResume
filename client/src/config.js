965af4b30ade629806d502b68f18c3bb09e39ddd
bishal tiwari <tiwaribishal517d@gmail.com>
2026-02-15 02:01:20 +0530

    commit

--- client/src/config.js
+++ client/src/config.js
@@ -4,7 +4,7 @@
 
 const API_URL = import.meta.env.VITE_API_URL || 
   (import.meta.env.MODE === 'production' 
-    ? 'https://your-backend-url.onrender.com'  // Replace this after deploying backend
+    ? 'https://myresume-vgdx.onrender.com'  // Replace this after deploying backend
     : 'http://localhost:5000')
 
 export default API_URL
